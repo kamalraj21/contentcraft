51f071aa15c23c146b1c628e3c65f6e33c6b612c
kamalraj21 <kamal.raj2181@gmail.com>
2024-11-03 20:55:22 +0530

    Updated astro config mjs file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()],
   site: 'https://kamalraj21.github.io',
-  base: '/contentcraft'
+  base: '/contentcraft',  // Make sure this matches exactly
+  integrations: [tailwind()]
 });
